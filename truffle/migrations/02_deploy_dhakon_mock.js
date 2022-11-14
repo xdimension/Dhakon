5fdcb6e852f714ccfdd78ee2772f5846c9e18e2a
xdimension <xdim222@gmail.com>
2022-11-14 15:54:10 +0700

    update migrations

--- truffle/migrations/02_deploy_dhakon_mock.js
+++ truffle/migrations/02_deploy_dhakon_mock.js
@@ -3,11 +3,17 @@ const { BigNumber } = require("@ethersproject/bignumber");
 const MockDhakon = artifacts.require("MockDhakon");
 
 module.exports = function (deployer) {
+  const callbackGasPrice = 1000000;   // Gas price limit for randomness callback (only change it if you know what you do!)
+
+  const ticketPrice = 0.1;            // The price of 1 ticket to join
+  const roundDays = 30;               // The round period (in days)
+
   deployer.deploy(
     MockDhakon, 
     "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
     "0x99aFAf084eBA697E584501b8Ed2c0B37Dd136693",
-    1000000,
-    BigNumber.from((0.1 * 10 ** 18).toString())
+    callbackGasPrice,
+    BigNumber.from((ticketPrice * 10 ** 18).toString()),
+    roundDays
   );
 };
