da0f755a8fdcd25a7cbc02573ce96c3ef2e39164
xdimension <xdim222@gmail.com>
2022-11-23 22:09:09 +0700

    update html

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -1,8 +1,7 @@
+import { Row, Col } from "react-bootstrap";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../assets/img/color-sharp.png"
 import { MailchimpForm } from "./MailchimpForm";
 
@@ -30,32 +29,28 @@ export const Stats = () => {
   return (
     <section className="stat" id="stats">
         <div className="container">
-            <div className="row">
-                <div className="col-12">
+            <Row>
+                <Col>
                     <div className="stat-bx wow zoomIn">
                         <h2>Statistic</h2>
-                        <p>Don't leave behind, take your chance and collect your Lucky Pot.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme stat-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                        </Carousel>
+                        <p>Don't be left behind, take your chance and collect your Lucky Pot.</p>
+                          <Row>
+                              <Col className="item" sm={12} md={4}>
+                                  <img src={meter1} alt="Image" />
+                                  <h5>Web Development</h5>
+                              </Col>
+                              <Col className="item" sm={12} md={4}>
+                                  <img src={meter2} alt="Image" />
+                                  <h5>Brand Identity</h5>
+                              </Col>
+                              <Col className="item" sm={12} md={4}>
+                                  <img src={meter3} alt="Image" />
+                                  <h5>Logo Design</h5>
+                              </Col>
+                        </Row>
                     </div>
-                </div>
-            </div>
+                </Col>
+            </Row>
             <MailchimpForm /> 
         </div>
         <img className="background-image-left" src={colorSharp} alt="Image" />
