5cf4eafc11c02ef9f03be9fe042cb8f6d5600584
xdimension <xdim222@gmail.com>
2022-11-25 12:22:44 +0700

    some clean up

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -6,26 +6,6 @@ import colorSharp from "../assets/img/color-sharp.png"
 import { MailchimpForm } from "./MailchimpForm";
 
 export const Stats = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
-
   return (
     <section className="stat" id="stats">
         <div className="container">
