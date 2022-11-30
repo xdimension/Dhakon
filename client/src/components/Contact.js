3c31262d128f22af7756626ab1ca2e639b08e767
xdimension <xdim222@gmail.com>
2022-11-30 22:03:54 +0700

    update Contact form

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -6,10 +6,8 @@ import TrackVisibility from 'react-on-screen';
 
 export const Contact = () => {
   const formInitialDetails = {
-    firstName: '',
-    lastName: '',
+    name: '',
     email: '',
-    phone: '',
     message: ''
   }
   const [formDetails, setFormDetails] = useState(formInitialDetails);
@@ -61,19 +59,13 @@ export const Contact = () => {
                 <h2>Get In Touch</h2>
                 <form onSubmit={handleSubmit}>
                   <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                    <Col size={12} sm={12} className="px-1">
+                      <input type="text" value={formDetails.name} placeholder="Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                     </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                    <Col size={12} sm={12} className="px-1">
+                      <input type="email" value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)} />
                     </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
+                    <Col size={12} sm={12} className="px-1">
                       <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                       <button type="submit"><span>{buttonText}</span></button>
                     </Col>
