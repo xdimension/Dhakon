daffa2484a251945aa2129776e2c38e2c2c469ea
xdimension <xdim222@gmail.com>
2022-12-03 16:13:48 +0700

    implement config and .env

--- backend/migrations/01_deploy_dhakon.js
+++ backend/migrations/01_deploy_dhakon.js
@@ -2,20 +2,16 @@ const { BigNumber } = require("@ethersproject/bignumber");
 
 const Dhakon = artifacts.require("Dhakon");
 
+const _config_ = require("../config").config
+
 module.exports = function (deployer) {
-  const callbackGasPrice = 1000000;   // Gas price limit for randomness callback (only change it if you know what you do!)
-
-  const ticketPrice = 0.1;            // The price of 1 ticket to join
-  const roundDays = 30;               // The round period (in days)
-  const commissionPct = 300;           // commissionPct = Commission Percentage * 100 (eg. 1.23% -> 1.23 * 100 = 123)
-
   deployer.deploy(
     Dhakon, 
-    "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",
-    "0x99aFAf084eBA697E584501b8Ed2c0B37Dd136693",
-    callbackGasPrice,
-    BigNumber.from((ticketPrice * 10 ** 18).toString()),
-    roundDays,
-    commissionPct
+    _config_.LINKTokenAddress,
+    _config_.VRFWrapperAddress,
+    _config_.callbackGasPrice,
+    BigNumber.from((_config_.ticketPrice * 10 ** 18).toString()),
+    _config_.roundDays,
+    _config_.commissionPct
   );
 };
--- client/src/scripts/contract.js
+++ client/src/scripts/contract.js
@@ -3,7 +3,7 @@ import jsonContract from '../contracts/MockDhakon.json';
 const contract = (web3) => {
   return new web3.eth.Contract(
     jsonContract.abi,
-    "0x7844F0d21d848575f6034b05526012D80725B998"
+    process.env.REACT_APP_CONTRACT_ADDRESS
   )
 }
   
