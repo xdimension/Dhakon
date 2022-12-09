612e3289fa0cfa8a53e28892e0121eb89051e325
xdimension <xdim222@gmail.com>
2022-12-09 10:52:48 +0700

    remove unused code

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import TrackVisibility from 'react-on-screen'
 import headerImg from "../assets/img/piggybank.png"
@@ -6,46 +5,6 @@ import { EnterPot } from "./EnterPot"
 import 'animate.css'
 
 export const Banner = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = [ "Lucky Day", "Good Day", "Best Moment" ];
-  const period = 2000;
-
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => { clearInterval(ticker) };
-  }, [text])
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
-  }
 
   return (
     <section className="banner" id="home">
