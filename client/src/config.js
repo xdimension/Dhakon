f792b80fc99b7cf8caee47c3bb84b4e0216620de
xdimension <xdim222@gmail.com>
2022-12-22 22:21:19 +0700

    add comment for info

--- client/src/config.js
+++ client/src/config.js
@@ -2,10 +2,10 @@ exports.config = {
 
     ticketPrice: 0.1,            // The price of 1 ticket to join
     token: {
-        name: 'Matic',
-        platform: 'Polygon',
-        shortName: 'MATIC', 
-        unitName: 'MATIC',
+        name: 'MATIC',           // 'Ether'   
+        platform: 'Polygon',     // 'Ethereum'
+        shortName: 'MATIC',      // 'Ether'
+        unitName: 'MATIC',       // 'ETH'
     },
     roundDays: 30,               // The round period (in days), max 255 days
     commissionPct: 250,          // commissionPct = Commission Percentage * 100 (eg. 2.5% -> 2.5 * 100 = 250)
