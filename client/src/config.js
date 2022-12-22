5801c9b6164074f0246639aa10d95fe927e7fad2
xdimension <xdim222@gmail.com>
2022-12-22 22:08:03 +0700

    change currency to token

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,11 @@
 exports.config = {
 
     ticketPrice: 0.1,            // The price of 1 ticket to join
-    currency: {
+    token: {
         name: 'Matic',
+        platform: 'Polygon',
         shortName: 'MATIC', 
-        unit: 'ether',
+        unitName: 'MATIC',
     },
     roundDays: 30,               // The round period (in days), max 255 days
     commissionPct: 250,          // commissionPct = Commission Percentage * 100 (eg. 2.5% -> 2.5 * 100 = 250)
