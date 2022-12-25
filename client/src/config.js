cfc406b311bfde54942db7b0069e7ad29d94cd0a
xdimension <xdim222@gmail.com>
2022-12-25 22:22:34 +0700

    move some configs from .env to config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,15 @@
 exports.config = {
+    appName: 'CrowdPot',
+    contract: {
+        address: '0xbE5091ca8eE651808758A8804eEA71F9A6B1F170',
+        jsonFile: 'Dhakon.json',
+    },
 
-    ticketPrice: 0.1,            // The price of 1 ticket to join
     network: {
         name: 'Polygon Mumbai',
         id: 80001
     },
+
     token: {
         name: 'MATIC',           // 'Ether'   
         platform: 'Polygon',     // 'Ethereum'
@@ -12,6 +17,8 @@ exports.config = {
         unitName: 'MATIC',       // 'ETH'
         explorerUrl: 'https://mumbai.polygonscan.com/tx/'
     },
+
+    ticketPrice: 0.1,            // The price of 1 ticket to join
     roundDays: 30,               // The round period (in days), max 255 days
     commissionPct: 250,          // commissionPct = Commission Percentage * 100 (eg. 2.5% -> 2.5 * 100 = 250)
  }
