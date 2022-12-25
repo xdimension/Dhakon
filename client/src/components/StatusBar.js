d9e9fbf002203f1a4b0a13fede5e8bb98e04ab0c
xdimension <xdim222@gmail.com>
2022-12-25 21:54:31 +0700

    show status for connect to correct network

--- client/src/components/StatusBar.js
+++ client/src/components/StatusBar.js
@@ -1,14 +1,23 @@
 import { Web3Context } from "./Web3Provider"
 import { useContext } from "react"
+import { GameContext } from "./GameProvider"
 
 export function StatusBar()
 {
-    const { hasWallet } = useContext(Web3Context)
+    const { networkId, hasWallet } = useContext(Web3Context)
+    const { config } = useContext(GameContext)
 
     return (
-          !hasWallet && 
+      <>
+          {!hasWallet && 
           <div className="top-status-bar">
             Please install <a href="https://metamask.io" target="_blank">Metamask</a> wallet to join
-          </div>
+          </div>}
+          
+          {networkId != 0 && networkId != config.network.id &&
+          <div className="top-status-bar">
+            You must connect to {config.network.name} network to join
+          </div>}
+      </>
     )
 }
