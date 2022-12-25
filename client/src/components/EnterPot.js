6820578ecb3e809faebd5ac0594f88dd077e2558
xdimension <xdim222@gmail.com>
2022-12-25 21:53:06 +0700

    add checking network

--- client/src/components/EnterPot.js
+++ client/src/components/EnterPot.js
@@ -5,34 +5,38 @@ import { Web3Context } from "./Web3Provider"
 
 export function EnterPot() 
 {
-    let { web3, vmContract, address, doRefresh } = useContext(Web3Context)
+    let { web3, networkId, vmContract, address, doRefresh } = useContext(Web3Context)
     let { config, numOfEntries, roundEndsAt } = useContext(GameContext)
 
     const enterPotHandler = async() => {
+        if (networkId && networkId != config.network.id) {
+            alert(`Please connect to ${config.network.name} network to join`)
+            return false;
+        }
 
-        if (vmContract && address) {
-
-            let remainingTime =  new Date(roundEndsAt * 1000) - Date.now();
-
-            if (numOfEntries > 0 && remainingTime <= 0) {
-                alert('This round has already ended.\nPlease wait for the next round')
-                window.location.reload()
-                return false;
-            }
-
-            try {
-                await vmContract.methods.enter()
-                    .send({
-                        from: address,
-                        value: web3.utils.toWei(config.ticketPrice.toString(), 'ether')
-                    })
-
-                doRefresh()
-            } catch(err) {
-                console.log(err.message)
-            }
-        } else {
+        if (!vmContract || !address) {
             alert('Please connect to wallet first')
+            return false;
+        }
+
+        let remainingTime =  new Date(roundEndsAt * 1000) - Date.now();
+
+        if (numOfEntries > 0 && remainingTime <= 0) {
+            alert('This round has already ended.\nPlease wait for the next round')
+            window.location.reload()
+            return false;
+        }
+
+        try {
+            await vmContract.methods.enter()
+                .send({
+                    from: address,
+                    value: web3.utils.toWei(config.ticketPrice.toString(), 'ether')
+                })
+
+            doRefresh()
+        } catch(err) {
+            console.log(err.message)
         }
     }
     
