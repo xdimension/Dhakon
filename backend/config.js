dc2ddccd796b07da1d64100472eaba8f0e6537fe
xdimension <xdim222@gmail.com>
2024-08-29 14:30:36 +0700

    move LINK_TOKEN_ADDR & VRF_WRAPPER_ADDR to env

--- backend/config.js
+++ backend/config.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 exports.config = {
 
     ticketPrice: 0.1,            // The price of 1 ticket to join
     roundDays: 30,               // The round period (in days), max 255 days
     commissionPct: 250,          // commissionPct = Commission Percentage * 100 (eg. 2.5% -> 2.5 * 100 = 250)
-    
-    LINKTokenAddress: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB",    // LINK token address
-    VRFWrapperAddress: "0x99aFAf084eBA697E584501b8Ed2c0B37Dd136693",   // VRF wrapper address
+
+    LINKTokenAddress: process.env['LINK_TOKEN_ADDR'],       // LINK token address
+    VRFWrapperAddress: process.env['VRF_WRAPPER_ADDR'],     // VRF wrapper address
 
     callbackGasPrice: 1000000,   // Gas price limit for randomness callback (only change it if you know what you do!)
 }
